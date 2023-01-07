0f053c99cf9a83179f029f6bbbbee5d853cd8ff0
Petr Tripolsky <tripolskypetr@gmail.com>
2023-01-07 23:47:58 +0300

    paymeny-gateway-contract

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # erc20-payment-gateway
 
 > A payment gateway for any `IERC20` tokens with testbed on top of [TestERC20](https://stackoverflow.com/questions/75043524/minimal-erc20-contract-for-testing/75043525#75043525) token
+
+## Contract source code
+
+```solidity
+contract PaymentGatewayContract {
+
+    IERC20 private erc20;
+    address owner;
+
+    uint256 public deployBlock;
+
+    constructor(address _erc20Address) {
+        erc20 = IERC20(_erc20Address);
+        owner = msg.sender;
+        deployBlock = block.number;
+    }
+
+    function sendUSDT(uint256 _amount, bytes32 _data) public {
+        require(erc20.balanceOf(msg.sender) >= _amount, "Insufficient funds");
+        erc20.transferFrom(msg.sender, owner, _amount);
+        emit Transfer(msg.sender, _amount, _data);
+    }
+
+    event Transfer(address indexed sender, uint256 amount, bytes32 _data);
+
+}
+```
---
+++ packages/client/src/contract/PaymentGatewayContract.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.7;
+
+interface IERC20 {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address to, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address from, address to, uint256 amount) external returns (bool);
+}
+
+contract PaymentGatewayContract {
+
+    IERC20 private erc20;
+    address owner;
+
+    uint256 public deployBlock;
+
+    constructor(address _erc20Address) {
+        erc20 = IERC20(_erc20Address);
+        owner = msg.sender;
+        deployBlock = block.number;
+    }
+
+    function sendUSDT(uint256 _amount, bytes32 _data) public {
+        require(erc20.balanceOf(msg.sender) >= _amount, "Insufficient funds");
+        erc20.transferFrom(msg.sender, owner, _amount);
+        emit Transfer(msg.sender, _amount, _data);
+    }
+
+    event Transfer(address indexed sender, uint256 amount, bytes32 _data);
+
+}
